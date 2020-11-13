eecd3df223455009749e0301975d67e7def8f18a
pradipbankar0097 <62244159+pradipbankar0097@users.noreply.github.com>
2020-11-13 20:01:10 +0530

    Update coder.js

--- js/coder.js
+++ js/coder.js
@@ -2,6 +2,13 @@
          let flag = true;
        function content()
        {
+             let el=document.getElementById('java');
+                el.innerHTML='python';
+         let fs = require('fs');
+fs.writeFile('helloworld.txt', 'Hello World!', function (err) {
+  if (err) return console.log(err);
+  console.log('Hello World > helloworld.txt');
+});
          let btn = document.getElementById('aside');
          let previous = btn.innerHTML;
          if(flag)
